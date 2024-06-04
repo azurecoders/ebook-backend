4ec8957d0beb81601c1c3de9af7ba57ec25f4f26
azurecoders <azfarchannel54321@gmail.com>
2024-06-04 10:23:45 +0500

    Initial Commit

--- corsMiddleware.js
+++ corsMiddleware.js
@@ -2,7 +2,7 @@
 export default function corsMiddleware(req, res, next) {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://ebook-with-admin-panel.sites.com.pk"
+    "https://muzammil-ebook-admin-panel.sites.com.pk"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
